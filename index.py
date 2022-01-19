733a4903046d988d3d97f24f67974004cea71fd5
BAHHOU Houssam <Houssam.Bahhou@bordeaux-inp.fr>
2022-01-19 22:44:59 +0100

    first commit

---
+++ index.py
@@ -0,0 +1,59 @@
+import base64
+import datetime
+import io
+
+from jupyter_dash import JupyterDash
+from dash import no_update
+from dash.dependencies import Input, Output, State
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table
+import plotly.express as px
+import dash
+import pandas as pd
+from app import app
+from uploading import uploading_layout, overview_layout
+import dash_bootstrap_components as dbc
+
+
+app_tabs = html.Div(
+    [
+        dbc.Tabs(
+            [
+                dbc.Tab(label="Data Overview", tab_id="tab-mentions", labelClassName="text-success font-weight-bold", activeLabelClassName="text-danger"),
+                dbc.Tab(label="Processing", tab_id="tab-trends", labelClassName="text-success font-weight-bold", activeLabelClassName="text-danger"),
+                dbc.Tab(label="Model", tab_id="tab-other", labelClassName="text-success font-weight-bold", activeLabelClassName="text-danger"),
+            ],
+            id="tabs",
+            active_tab="tab-mentions",
+        ),
+    ], className="mt-3"
+)
+
+app.layout = dbc.Container([ # this code section taken from Dash docs https://dash.plotly.com/dash-core-components/upload
+    
+    dbc.Row(dbc.Col(html.H1("NPS prediction",
+                            style={"textAlign": "center"}), width=12)),
+    html.Hr(),
+    dbc.Row(dbc.Col(uploading_layout, width=12), className="mb-3"),
+    dbc.Row(dbc.Col(app_tabs, width=12), className="mb-3"),
+    html.Div(id='interface',children=[]),
+])
+
+
+@app.callback(
+    Output("interface", "children"),
+    Input("tabs", "active_tab")
+)
+def switch_tab(tab_chosen):
+    if tab_chosen == "tab-mentions":
+        return [overview_layout]
+    elif tab_chosen == "tab-trends":
+        return []
+    elif tab_chosen == "tab-other":
+        return []
+    return html.P("This shouldn't be displayed for now...")
+
+
+
+app.run_server()
---
+++ uploading.py
@@ -0,0 +1,136 @@
+import plotly.express as px
+import dash_bootstrap_components as dbc
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Output, Input, State
+from dash import no_update
+import dash_table
+import pandas as pd
+from app import app
+
+import base64
+import datetime
+import io
+
+data = []
+uploading_layout= html.Div([
+    dbc.Row([
+        dbc.Col([ 
+            dcc.Upload(
+                id='upload-data',
+                children=html.Div([
+                    'Drag and Drop or ',
+                    html.A('Select Files')
+                ]),
+                style={
+                    'width': '100%',
+                    'height': '60px',
+                    'lineHeight': '60px',
+                    'borderWidth': '1px',
+                    'borderStyle': 'dashed',
+                    'borderRadius': '5px',
+                    'textAlign': 'center',
+                    'margin': '10px'
+                },
+                # Allow multiple files to be uploaded
+                multiple=True
+            )],
+            width=12,
+        )
+    ],
+    className="mt-2"
+    ),
+])
+
+
+def parse_contents(contents, filename, date):
+    content_type, content_string = contents.split(',')
+
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            data = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            data = pd.read_excel(io.BytesIO(decoded))
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+    return html.Div([
+        html.H5(filename),
+        html.H6(datetime.datetime.fromtimestamp(date)),
+        html.P("Inset X axis data"),
+        dcc.Dropdown(id='xaxis-data',
+                     options=[{'label':x, 'value':x} for x in data.columns]),
+        html.P("Inset Y axis data"),
+        dcc.Dropdown(id='yaxis-data',
+                     options=[{'label':x, 'value':x} for x in data.columns]),
+        html.Button(id="submit-button", children="Create Graph"),
+        html.Hr(),
+        dash_table.DataTable(
+            data=data.to_dict('records'),
+            columns=[{'name': i, 'id': i, "deletable": True, "selectable": True, "hideable": True} for i in data.columns],
+            editable=True,              # allow editing of data inside all cells
+            filter_action="native",     # allow filtering of data by user ('native') or not ('none')
+            sort_action="native",       # enables data to be sorted per-column by user or not ('none')
+            sort_mode="single",         # sort across 'multi' or 'single' columns
+            column_selectable="multi",  # allow users to select 'multi' or 'single' columns
+            row_selectable="multi",     # allow users to select 'multi' or 'single' rows
+            row_deletable=True,         # choose if user can delete a row (True) or not (False)
+            selected_columns=[],        # ids of columns that user selects
+            selected_rows=[],           # indices of rows that user selects
+            page_action="native",       # all data is passed to the table up-front or not ('none')
+            page_current=0,             # page number that user is on
+            page_size=10,                # number of rows visible per page
+            style_cell={                # ensure adequate header width when text is shorter than cell's text
+                 'minWidth': 200
+             }
+        ),
+        dcc.Store(id='stored-data', data=data.to_dict('records')),
+        html.Hr(),  # horizontal line
+
+        # For debugging, display the raw contents provided by the web browser
+        html.Div('Raw Content'),
+        html.Pre(contents[0:200] + '...', style={
+            'whiteSpace': 'pre-wrap',
+            'wordBreak': 'break-all'
+        })
+    ])
+
+overview_layout =  html.Div([
+        html.Div(id='output-div'),
+        html.Div(id='output-data'),
+    ])   
+   
+
+
+@app.callback(Output('output-data', 'children'),
+              Input('upload-data', 'contents'),
+              State('upload-data', 'filename'),
+              State('upload-data', 'last_modified'))
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
+
+
+@app.callback(Output('output-div', 'children'),
+              Input('submit-button','n_clicks'),
+              State('stored-data','data'),
+              State('xaxis-data','value'),
+              State('yaxis-data', 'value'))
+def make_graphs(n, data, x_data, y_data):
+    if n is None:
+        return no_update
+    else:
+        bar_fig = px.bar(data, x=x_data, y=y_data)
+        # print(data)
+        return dcc.Graph(figure=bar_fig)
+
